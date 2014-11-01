bead7ea7450346ef37ed5d371533478026849b8a
Jay Bardhan <jbardhan@alum.mit.edu>
2014-11-01 01:23:53 -0400

    Tyrosine example had some booted panels, so this change removes panels with < 1e-6 area

--- loadSrfIntoPanels.m
+++ loadSrfIntoPanels.m
@@ -1,8 +1,19 @@
 function surfdata = loadSrfIntoPanels(srf)
 
+areaThreshold = 1e-6;
+
 [meshBase,rootDir] = readsrf(srf);
 meshData = readmesh(meshBase,1,1);
 [centroids, normals, areas] = genmeshcolloc(meshData);
+
+I = find(areas > areaThreshold);
+areas = areas(I);
+centroids = centroids(I,:);
+normals = normals(I,:);
+meshData.face=meshData.face(I,:);
+meshData.X = meshData.X(:,I);
+meshData.Y = meshData.Y(:,I);
+meshData.Z = meshData.Z(:,I);
 
 surfdata = struct('meshData',meshData,'areas',areas, 'centroids',centroids, ...
 		  'normals',normals);
