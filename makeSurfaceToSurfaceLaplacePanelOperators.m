f9920482e6c5be53d364a6cba4506a0ef0898c77
Jay Bardhan <jbardhan@alum.mit.edu>
2014-12-06 17:10:19 -0500

    Added capacity to have use different source and destination surfaces.

--- makeSurfaceToSurfaceLaplacePanelOperators.m
+++ makeSurfaceToSurfaceLaplacePanelOperators.m
@@ -1,6 +1,12 @@
-function operstruct = makeSurfaceToSurfaceLaplacePanelOperators(surf)
+function operstruct = makeSurfaceToSurfaceLaplacePanelOperators(surf, ...
+						  surf2)
 
-[V, K, Kp] = colloc_Laplace(surf.meshData, surf.centroids, surf.normals, ...
-			surf.areas);
+if nargin < 2
+  [V, K, Kp] = colloc_Laplace(surf.meshData, surf.centroids, surf.normals, ...
+			      surf.areas);
+else
+  [V, K, Kp] = colloc_Laplace(surf.meshData, surf2.centroids, ...
+			      surf2.normals, surf2.areas);
+end
 
 operstruct = struct('V',V, 'K', K, 'Kp', Kp);
--- makeSurfaceToSurfaceYukawaPanelOperators.m
+++ makeSurfaceToSurfaceYukawaPanelOperators.m
@@ -1,6 +1,12 @@
-function operstruct = makeSurfaceToSurfaceYukawaPanelOperators(surf,kappa)
+function operstruct = makeSurfaceToSurfaceYukawaPanelOperators(surf,kappa,surf2)
 
-[V, K] = colloc_Yukawa(surf.meshData, surf.centroids, surf.normals, ...
-			surf.areas,kappa);
+
+if nargin < 3
+  [V, K] = colloc_Yukawa(surf.meshData, surf.centroids, surf.normals, ...
+			 surf.areas,kappa);
+else
+  [V, K] = colloc_Yukawa(surf.meshData, surf2.centroids, surf2.normals, ...
+			 surf2.areas, kappa);
+end
 
 operstruct = struct('V',V, 'K', K);
